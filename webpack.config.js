c2c1bef3b69b2852f51ea2c219999fda96c2dddd
Thomas Guesnon <>
2024-12-12 18:35:16 +0100

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: ['./src/js/index.ts', './src/sass/index.scss'],
+  entry: ['./src/ts/index.ts', './src/sass/index.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'js/bundle.js'

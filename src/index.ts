2b9d4bf510f3d4efb51904543d27bedb395ad1ce
Thomas Guesnon <>
2024-12-12 22:47:12 +0100

    still does not work

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import generate from "./generate";
 
 export default function generateColorPalette(color:string){
     const colorsPalette:object = generate(color);
-    // console.log(colorsPalette);
     return colorsPalette;
 }
